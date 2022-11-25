f192a48c011bf7877515c4a843a670a13968b9d1
RENaa_FD <1593713272@qq.com>
2022-11-25 17:17:12 +0800

    添加还原模式和帮助

--- 测试模式.bat
+++ 测试模式.bat
@@ -3,11 +3,12 @@ title 任渊生存-测试模式
 cd /d "%~dp0"
 cls
 
-call :info 初始化中...
+:: 帮助模式参数检查
+if "%~1" == "-?" ( goto HelpMode )
+if "%~1" == "-？" ( goto HelpMode )
+if "%~1" == "-help" ( goto HelpMode )
 
-:: 付费版参数检查
-if "%~1" == "-pro" (set folder=RYSurvival-Pro && call :info 检测到使用付费版仓库) else (set folder=RYSurvival && call :info 检测到使用免费版仓库)
-set folder=%folder: =%
+call :info 初始化中...
 
 :: 彩色字体初始化
 setlocal EnableDelayedExpansion
@@ -15,13 +16,20 @@ for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1)
 call :info 彩色字体已完成初始化
 cd..
 
+:: 还原模式参数检查
+if "%~1" == "-res" ( call :RestoreMode && goto loop )
+
+:: 付费版参数检查
+if "%~1" == "-pro" ( set folder=RYSurvival-Pro && call :info 检测到使用付费版仓库 ) else ( set folder=RYSurvival && call :info 检测到使用免费版仓库 )
+set folder=%folder: =%
+
 :: 读取服务端版本
 if exist %folder% (cd %folder%) else call :NotFoundError %folder% folder
 call :info 已找到服务端仓库文件夹
 if not exist .version call :NotFoundError .version file
 call :info 已找到版本识别文件
 :: 读取.version文件中的版本信息
-for /f "tokens=1,* delims==" %%a in ('findstr "Version=" ".version"') do set version=%%b
+for /f "tokens=1,* delims==" %%a in (' findstr "Version=" ".version" ') do set version=%%b
 if "%version%" == "" call :InvalidParameterError version
 :: 清除空格
 set version=%version: =%
@@ -31,11 +39,11 @@ cd /d "%~dp0"
 
 :: 检查运行库
 call :info 检查运行库中
-if exist test-environment-runtime (cd test-environment-runtime) else call :NotFoundError test-environment-runtime folder
+if exist test-environment-runtime ( cd test-environment-runtime ) else call :NotFoundError test-environment-runtime folder
 call :info 已找到运行库文件夹
 if not exist java\bin\java.exe call :NotFoundError java.exe file
 call :info 已找到Java
-if exist test-flies (cd test-flies) else call :NotFoundError test-flies folder
+if exist test-flies ( cd test-flies ) else call :NotFoundError test-flies folder
 call :info 已找到测试文件夹
 
 :: 检查版本文件夹是否存在
@@ -43,7 +51,7 @@ if not exist TestServerLib_%version% call :NotFoundError TestServerLib_%version%
 call :info 已找到测试文件
 
 :: 读取对应版本的核心
-for /f "tokens=1,* delims==" %%a in ('findstr "%version%=" "version.properties"') do set core-name=%%b
+for /f "tokens=1,* delims==" %%a in (' findstr "%version%=" "version.properties" ') do set core-name=%%b
 if "%core-name%" == "" call :InvalidParameterError core-name
 :: 清除空格
 set core-name=%core-name: =%
@@ -67,14 +75,18 @@ cd RYSurvival-TestServer
 :: 扩展包参数检查
 if "%~1" == "-ext" ( call :ExtensionPack ) else if "%~2" == "-ext" ( call :ExtensionPack )
 
+:: 添加版本信息
+echo core-name=%core-name% >restore.properties
+echo version=%version% >>restore.properties
+
 :: 初始化完成
 call :info 初始化完成
 
 :: 启动服务端
 call :info 按任意键启动服务端 && pause>nul
-:: 设置标题
-title 任渊生存服务端-测试模式 %core-name:-=%
 
+:: 设置标题
+call :title
 
 :: 脚本主循环
 :loop
@@ -110,13 +122,17 @@ goto loop
 echo [Info] %*
 goto exit
 
+
 :warning
 call :colortext 0e "[Warning] %~1" && echo.
 goto exit
 
+
 :error
 call :colortext 0c "[Error] %~1" && echo.
 goto exit
+
+
 
 :: 输出彩色字体
 :colortext
@@ -127,6 +143,13 @@ del "%~2" > nul 2>&1
 goto exit
 
 
+:: 设置标题
+:title
+title 任渊生存服务端-测试模式 %core-name:-=%
+goto exit
+
+
+
 :: 错误报告处理
 :NotFoundError
 if "%~2" == "folder" call :error "NotFoundError：无法找到对应文件夹 %~1"
@@ -134,10 +157,13 @@ if "%~2" == "file" call :error "NotFoundError：无法找到对应文件 %~1"
 pause>nul
 exit 
 
+
 :InvalidParameterError
 call :error "InvalidParameterError：无效参数 %~1"
 pause>nul
 exit 
+
+
 
 :: 扩展包处理
 :ExtensionPack
@@ -148,12 +174,64 @@ rd .extensionpack /s/q
 call :info 已装载扩展包
 goto exit
 
+
 :: 检测到非服务端的退出
 :non-version-exit
 call :info 无服务端,即将退出
 ping -n 3 -w 500 0.0.0.1 > nul
 goto exit
 
+
+:: 还原模式处理
+:RestoreMode
+call :info 还原模式已启用
+
+:: 初始化文件位置
+cd /d "%~dp0"
+cd..
+if not exist RYSurvival-TestServer call :NotFoundError RYSurvival-TestServer folder
+cd RYSurvival-TestServer
+if not exist restore.properties call :NotFoundError restore.properties file
+
+:: 读取版本信息
+for /f "tokens=1,* delims==" %%a in (' findstr "version=" "restore.properties" ') do set version=%%b
+if "%version%" == "" call :InvalidParameterError version
+:: 清除空格
+set version=%version: =%
+call :info 读取到服务端版本 %version%
+
+:: 读取核心名称
+for /f "tokens=1,* delims==" %%a in (' findstr "core-name=" "restore.properties" ') do set core-name=%%b
+if "%core-name%" == "" call :InvalidParameterError core-name
+:: 清除空格
+set core-name=%core-name: =%
+call :info 读取到服务端核心名称 %core-name%
+
+:: 设置标题
+call :title
+
+:: 加载完成
+call :info 已完成还原模式加载
+call :info 按任意键继续 && pause>nul
+
+goto exit
+
+
+:: 帮助模式
+:HelpMode
+call :info 任渊生存-帮助
+call :info 用法(优先级从上到下排序):
+call :info
+call :info %~n0 -?   获取此帮助
+call :info %~n0 -help   获取此帮助
+call :info %~n0 -res   还原至上次测试模式
+call :info %~n0 -pro   切换至付费版
+call :info %~n0 -ext   装载扩展包
+call :info %~n0 -pro -ext   切换至付费版并装载扩展包
+call :info
+call :info 按任意键退出
+pause>nul
+goto exit
+
+:: 退出标识,请不要在此下方添加代码
 :exit
-
-
